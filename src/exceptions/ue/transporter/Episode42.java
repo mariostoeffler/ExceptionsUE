a768662608809aeebf2bcb64ef5753069ecb2bc2
StoefflerMario <stoeffler@mario.at>
2023-03-04 15:41:56 +0100

    new classes added;

--- src/exceptions/ue/transporter/Episode42.java
+++ src/exceptions/ue/transporter/Episode42.java
@@ -1,4 +1,8 @@
 package exceptions.ue.transporter;
 
 public class Episode42 {
+
+    public static void main(String[] args) {
+        Starship enterprise = new Starship("Captain Kirk");
+    }
 }
--- src/exceptions/ue/transporter/Starship.java
+++ src/exceptions/ue/transporter/Starship.java
@@ -5,6 +5,11 @@ public class Starship {
     private String name;
     private Transporter transporter;
 
+    public Starship(String name, Transporter transporter) {
+        this.name = name;
+        this.transporter = transporter;
+    }
+
     public void beamUp(String person, String from){
 
     }
